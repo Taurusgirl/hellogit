60ab899e2945afb7b96ccf4c4ce4d55146c919fc
Guest <taurusgirl@users.noreply.github.com>
2017-09-20 15:24:27 -0400

    Added implementation file

---
+++ course.h
@@ -0,0 +1,16 @@
+#ifndef COURSE_H
+#define COURSE_H
+
+class course
+{
+private:
+string word
+int code
+
+public:
+course( string a, int c);
+string getWord();
+void setWord (string);
+int getCode();
+void setCode(int);
+};
