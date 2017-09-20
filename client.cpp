36d16893cfb2d154df9f882f3b18d19194850a7a
Guest <taurusgirl@users.noreply.github.com>
2017-09-20 15:35:46 -0400

    Added client file

---
+++ client.cpp
@@ -0,0 +1,17 @@
+//client
+
+#include<vector>
+#include <iostream>
+using namespace std;
+
+int main()
+
+vector <course> v1={{"comp", 1210},{"comp", 1205},{"foun",1001},{"foun",1020}}
+
+
+for (auto v:v1)
+{
+coput<<v.getWord()<,""v.getCODE()<,ENDL;
+}
+cout<<endl;
+}
---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "course.h"
+using namespace std;
+
+course::Course (string a, intc)
+{
+word =a
+code=c
+}
+
+course::getWord ()
+{
+return word;
+}
+
+void course::setWord (stringa)
+{
+word=a;
+}
+
+int course::getCode ()
+{
+return code;
+}
+void course::setCode(int c)
+{code=c;
+}
